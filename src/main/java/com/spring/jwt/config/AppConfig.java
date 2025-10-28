1cb06b2378f00ab94fad527ddfce10d004991528
tkraj888 <tusharck88@gmail.com>
2025-10-28 16:27:52 +0530

    new cors m

--- src/main/java/com/spring/jwt/config/AppConfig.java
+++ src/main/java/com/spring/jwt/config/AppConfig.java
@@ -116,7 +116,7 @@ public class AppConfig {
             @Override
             public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                 CorsConfiguration config = new CorsConfiguration();
-                config.setAllowedOrigins(Arrays.asList("https://license01.netlify.app/","https://license01.netlify.app","http://localhost:5173","https://licence-tau.vercel.app","https://dostenterprises.com/","https://licences01.netlify.app"));
+                config.setAllowedOrigins(Arrays.asList("https://licenceok.vercel.app","https://license01.netlify.app/","https://license01.netlify.app","http://localhost:5173","https://licence-tau.vercel.app","https://dostenterprises.com/","https://licences01.netlify.app"));
 //                config.setAllowedOrigins(Arrays.asList("https://license01.netlify.app/","https://license01.netlify.app"));
                 config.setAllowedMethods(Collections.singletonList("*"));
                 config.setAllowCredentials(true);
