fe4442b34fc1527be21b3b45aed994f55bb80bb1
tkraj888 <tusharck88@gmail.com>
2025-11-22 20:29:14 +0530

    prod add newall okl

--- src/main/java/com/spring/jwt/config/filter/CorsFilter.java
+++ src/main/java/com/spring/jwt/config/filter/CorsFilter.java
@@ -7,25 +7,57 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 
 import java.io.IOException;
+import java.util.List;
 
 @Configuration
 public class CorsFilter implements Filter {
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+//    @Override
+//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+//            throws IOException, ServletException {
+//        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+//        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+//
+//        httpServletResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+//        httpServletResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+//        httpServletResponse.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+//        httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
+//        if (httpServletRequest.getMethod().equalsIgnoreCase("OPTIONS")) {
+//            httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+//        } else {
+//            chain.doFilter(request, response);
+//        }
+//    }
+@Override
+public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+        throws IOException, ServletException {
 
-        httpServletResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-        httpServletResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
-        httpServletResponse.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
-        httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
-        if (httpServletRequest.getMethod().equalsIgnoreCase("OPTIONS")) {
-            httpServletResponse.setStatus(HttpServletResponse.SC_OK);
-        } else {
-            chain.doFilter(request, response);
-        }
+    HttpServletResponse res = (HttpServletResponse) response;
+    HttpServletRequest req = (HttpServletRequest) request;
+
+    String origin = req.getHeader("Origin");
+
+    List<String> allowedOrigins = List.of(
+            "http://localhost:5173",
+            "https://www.dostenterprises.com",
+            "https://dost02.dostenterprises.com"
+    );
+
+    if (allowedOrigins.contains(origin)) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
     }
+
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "3600");
+
+    if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
+        res.setStatus(HttpServletResponse.SC_OK);
+        return;
+    }
+
+    chain.doFilter(request, response);
+}
 
     @Override
     public void destroy() {
